499f3ce2fad12b0a8d151cb1b060a94c902f014a
IurydJR <iury.djr.ti@gmail.com>
2024-06-28 11:48:12 -0300

    add sales-dashboard component

---
+++ src/app/components/sales-dashboard/sales-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-dashboard.component.html',
+  styleUrl: './sales-dashboard.component.scss'
+})
+export class SalesDashboardComponent {
+
+}

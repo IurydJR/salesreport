cbd98604e2870e193ea3aadac07ac391389703ab
IurydJR <iury.djr.ti@gmail.com>
2024-06-29 05:12:19 -0300

    feat: create routes to news components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { SalesDashboardComponent } from './components/sales-dashboard/sales-dashboard.component';
 import { SalesListComponent } from './components/sales-list/sales-list.component';
+import { SalesManegementComponent } from './components/sales-manegement/sales-manegement.component';
+import { UserManegementComponent } from './components/user-manegement/user-manegement.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {path:'', component:SalesDashboardComponent},
-  {path:'list', component:SalesListComponent}
+  {path:'list', component:SalesListComponent},
+  {path:'sale', component:SalesManegementComponent},
+  {path:'user', component:UserManegementComponent},
+  {path:'login', component:LoginComponent},
 ];

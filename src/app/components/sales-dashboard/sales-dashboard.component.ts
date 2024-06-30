42a810d17bd1ea9b0cada7fab6773586a8937807
IurydJR <iury.djr.ti@gmail.com>
2024-06-30 10:21:07 -0300

    feat: implements the class dataSales to sales-dashboard component

--- src/app/components/sales-dashboard/sales-dashboard.component.ts
+++ src/app/components/sales-dashboard/sales-dashboard.component.ts
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { concatMap, of } from 'rxjs';
 import { Observable } from 'rxjs';
+import { DataSales } from '../../models/dataSales';
 
 @Component({
   selector: 'app-sales-dashboard',
@@ -27,10 +28,7 @@ import { Observable } from 'rxjs';
 export class SalesDashboardComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
 
-  public sales: any[] = [];
-  public categories: any[] = [];
-  public products: any[] = [];
-  public brands: any[] = [];
+  public dataSales = new DataSales([], [], [], []);
   public categoryId:number = 0; 
   public productId:number = 0; 
   public brandId:number = 0;
@@ -52,7 +50,7 @@ export class SalesDashboardComponent implements OnInit {
 
       const blueGradient = ctx.createLinearGradient(0, 0, 0, height);
       blueGradient.addColorStop(0, "#38caff"); 
-      blueGradient.addColorStop(1, "##104db0"); 
+      blueGradient.addColorStop(1, "#104db0"); 
 
       const greenGradient = ctx.createLinearGradient(0, 0, 0, height);
       greenGradient.addColorStop(0, "#63f549");       
@@ -91,12 +89,12 @@ export class SalesDashboardComponent implements OnInit {
   public barChartType = 'bar' as const;
 
   public barChartData: ChartData<'bar'> = {
-    labels: [],
+    labels: [""],
     datasets: [
-      { data: [22], label: 'January', backgroundColor: 'blue' },
-      { data: [2], label: 'February', backgroundColor: 'green'},
-      { data: [9], label: 'March', backgroundColor: 'yellow'},
-      { data: [1], label: 'April', backgroundColor: 'orange'}
+      { data: [], label: 'January', backgroundColor: 'blue' },
+      { data: [], label: 'February', backgroundColor: 'green'},
+      { data: [], label: 'March', backgroundColor: 'yellow'},
+      { data: [], label: 'April', backgroundColor: 'orange'}
     ],
   };
 
@@ -110,40 +108,20 @@ export class SalesDashboardComponent implements OnInit {
 
   loadData(): void {
     this.salesService.getAll().pipe(
-      concatMap(categories => this.setCategories(categories)),
-      concatMap(() => this.setProducts(this.categories[0].products)),
-      concatMap(() => this.setBrands(this.products[0].brands)),
-      concatMap(() => this.setSales(this.brands[0].sales))
+      concatMap(categories => this.dataSales.categories = categories),
+      concatMap(() => this.dataSales.products = this.dataSales.categories[0].products),
+      concatMap(() => this.dataSales.brands = this.dataSales.products[0].brands),
+      concatMap(() => this.dataSales.sales = this.dataSales.brands[0].sales)
     ).subscribe(() => { this.updateGraph() });
-  }
-
-  setCategories(categories: any[]): Observable<any> {
-    this.categories = categories;
-    return of(categories);
-  }
-
-  setProducts(products: any[]): Observable<any> {
-    this.products = products;
-    return of(products);
-  }
-
-  setBrands(brands: any[]): Observable<any> {
-    this.brands = brands;
-    return of(brands);
-  }
-
-  setSales(sales: any[]): Observable<any> {
-    this.sales = sales;
-    return of(sales);
   }
 
   onCategoryChange(event: any): void {
     this.categoryId = Number(event.target.value);
-    const selectedCategory = this.categories.find(cat => Number(cat.id) === this.categoryId);
+    const selectedCategory = this.dataSales.categories.find(cat => Number(cat.id) === this.categoryId);
     if (selectedCategory) {
-      this.setProducts(selectedCategory.products).subscribe(() => {
-        this.setBrands(this.products[0].brands).subscribe(() => {
-          this.setSales(this.brands[0].sales).subscribe(() => {
+      this.dataSales.products = selectedCategory.products.subscribe(() => {
+        this.dataSales.brands = this.dataSales.products[0].brands.subscribe(() => {
+          this.dataSales.sales = this.dataSales.brands[0].sales.subscribe(() => {
             this.updateGraph();
           });
         });
@@ -153,10 +131,10 @@ export class SalesDashboardComponent implements OnInit {
 
   onProductChange(event: any): void {
     this.productId = Number(event.target.value); 
-    const selectedProduct = this.products.find(prod => Number(prod.id) === this.productId);
+    const selectedProduct = this.dataSales.products.find(prod => Number(prod.id) === this.productId);
     if (selectedProduct) {
-      this.setBrands(selectedProduct.brands).subscribe(() => {
-        this.setSales(this.brands[0].sales).subscribe(() => {
+      this.dataSales.brands = selectedProduct.brands.subscribe(() => {
+        this.dataSales.sales = this.dataSales.brands[0].sales.subscribe(() => {
           this.updateGraph();
         });
       });
@@ -165,19 +143,19 @@ export class SalesDashboardComponent implements OnInit {
 
   onBrandChange(event: any): void {
     this.brandId = Number(event.target.value); 
-    const selectedBrand = this.brands.find(brand => Number(brand.id) === this.brandId);
+    const selectedBrand = this.dataSales.brands.find(brand => Number(brand.id) === this.brandId);
     if (selectedBrand) {
-      this.setSales(selectedBrand.sales).subscribe(() => {
+      this.dataSales.sales = selectedBrand.sales.subscribe(() => {
         this.updateGraph();
       });
     }
   }
 
   updateGraph(): void {
-    this.barChartData.datasets[0].data = [this.sales[0].quantity];
-    this.barChartData.datasets[1].data = [this.sales[1].quantity];
-    this.barChartData.datasets[2].data = [this.sales[2].quantity];
-    this.barChartData.datasets[3].data = [this.sales[3].quantity];
+    this.barChartData.datasets[0].data = [this.dataSales.sales[0].quantity];
+    this.barChartData.datasets[1].data = [this.dataSales.sales[1].quantity];
+    this.barChartData.datasets[2].data = [this.dataSales.sales[2].quantity];
+    this.barChartData.datasets[3].data = [this.dataSales.sales[3].quantity];
     if (this.chart) {
       this.chart.update();
     }

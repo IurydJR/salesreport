46fc6a8e84334c8a9362056b9e9d09db1be70cef
IurydJR <iury.djr.ti@gmail.com>
2024-06-30 00:48:43 -0300

    refactor: refactor load sales data funciton

--- src/app/components/sales-dashboard/sales-dashboard.component.ts
+++ src/app/components/sales-dashboard/sales-dashboard.component.ts
@@ -1,21 +1,29 @@
-import { Component, ViewChild, OnInit  } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { SalesService } from '../../services/sales.service';
 import { NgFor } from '@angular/common';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { concatMap, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sales-dashboard',
   standalone: true,
-  imports: [BaseChartDirective, NgFor, FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule],
+  imports: [
+    BaseChartDirective, 
+    NgFor, 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatInputModule,
+  ],
   templateUrl: './sales-dashboard.component.html',
   styleUrl: './sales-dashboard.component.scss'
 })
-
 export class SalesDashboardComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
 
@@ -42,7 +50,6 @@ export class SalesDashboardComponent implements OnInit {
       const ctx = this.chart.chart.ctx;
       const height = this.chart.chart.height;
 
-      // Gradient color for chart bar
       const blueGradient = ctx.createLinearGradient(0, 0, 0, height);
       blueGradient.addColorStop(0, "#38caff"); 
       blueGradient.addColorStop(1, "##104db0"); 
@@ -68,16 +75,11 @@ export class SalesDashboardComponent implements OnInit {
     }
   }
 
-  public category: any[] = [];
-  public product: any[] = [];
-  public brand: any[] = [];
-
-  // Chart options configuration
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     scales: {
       x: {},
       y: {
-        min: 10,
+        min: 0,
       },
     },
     plugins: {
@@ -91,69 +93,61 @@ export class SalesDashboardComponent implements OnInit {
   public barChartData: ChartData<'bar'> = {
     labels: [],
     datasets: [
-      { data: [], label: 'January', backgroundColor: 'blue' },
-      { data: [], label: 'February', backgroundColor: 'green'},
-      { data: [], label: 'March', backgroundColor: 'yellow'},
-      { data: [], label: 'April', backgroundColor: 'orange'}
+      { data: [22], label: 'January', backgroundColor: 'blue' },
+      { data: [2], label: 'February', backgroundColor: 'green'},
+      { data: [9], label: 'March', backgroundColor: 'yellow'},
+      { data: [1], label: 'April', backgroundColor: 'orange'}
     ],
   };
 
-  // Events to chart
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: object[];
-  }): void {
+  public chartClicked({ event, active }: { event?: ChartEvent; active?: object[] }): void {
     console.log(event, active);
   }
 
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: object[];
-  }): void {
+  public chartHovered({ event, active }: { event?: ChartEvent; active?: object[] }): void {
     console.log(event, active);
   }
 
-  // Load data from Api to fill select input
   loadData(): void {
-    this.loadCategories();
+    this.salesService.getAll().pipe(
+      concatMap(categories => this.setCategories(categories)),
+      concatMap(() => this.setProducts(this.categories[0].products)),
+      concatMap(() => this.setBrands(this.products[0].brands)),
+      concatMap(() => this.setSales(this.brands[0].sales))
+    ).subscribe(() => { this.updateGraph() });
   }
 
-  loadCategories(): void {
-    this.salesService.getAll().subscribe(data => {
-      this.categories = data;
-      
-      if (this.categories.length > 0) {
-        this.loadProducts(this.categories[0].products);
-      }
-    });
+  setCategories(categories: any[]): Observable<any> {
+    this.categories = categories;
+    return of(categories);
   }
 
-  loadProducts(products: any[]): void {
+  setProducts(products: any[]): Observable<any> {
     this.products = products;
-
-    if (this.products.length > 0) {
-      this.loadBrands(this.products[0].brands);
-    }
+    return of(products);
   }
 
-  loadBrands(brands: any[]): void {
+  setBrands(brands: any[]): Observable<any> {
     this.brands = brands;
+    return of(brands);
   }
 
-  // Update the another select values and the chart data when a select value changes 
+  setSales(sales: any[]): Observable<any> {
+    this.sales = sales;
+    return of(sales);
+  }
+
   onCategoryChange(event: any): void {
     this.categoryId = Number(event.target.value);
     const selectedCategory = this.categories.find(cat => Number(cat.id) === this.categoryId);
     if (selectedCategory) {
-      this.loadProducts(selectedCategory.products);
-      this.sales = selectedCategory.products[0].brands[0].sales;
-      this.updateGraph()
+      this.setProducts(selectedCategory.products).subscribe(() => {
+        this.setBrands(this.products[0].brands).subscribe(() => {
+          this.setSales(this.brands[0].sales).subscribe(() => {
+            this.updateGraph();
+          });
+        });
+      });
     }
   }
 
@@ -161,34 +155,31 @@ export class SalesDashboardComponent implements OnInit {
     this.productId = Number(event.target.value); 
     const selectedProduct = this.products.find(prod => Number(prod.id) === this.productId);
     if (selectedProduct) {
-      this.loadBrands(selectedProduct.brands);
-      this.sales = selectedProduct.brands[0].sales;
-      this.updateGraph()
+      this.setBrands(selectedProduct.brands).subscribe(() => {
+        this.setSales(this.brands[0].sales).subscribe(() => {
+          this.updateGraph();
+        });
+      });
     }
-    let i = 0;
   }
 
   onBrandChange(event: any): void {
     this.brandId = Number(event.target.value); 
-    const selectedBrand = this.brands.find(prod => Number(prod.id) === this.brandId);
+    const selectedBrand = this.brands.find(brand => Number(brand.id) === this.brandId);
     if (selectedBrand) {
-      this.sales = selectedBrand.sales;
-      this.updateGraph()
+      this.setSales(selectedBrand.sales).subscribe(() => {
+        this.updateGraph();
+      });
     }
   }
- 
-  // Update chart data with updating data sale
-  updateGraph() {
-    this.barChartData.datasets[0].data = [this.sales[0]?.quantity || 0]
-    this.barChartData.datasets[1].data = [this.sales[1]?.quantity || 0]
-    this.barChartData.datasets[2].data = [this.sales[2]?.quantity || 0]
-    this.barChartData.datasets[3].data = [this.sales[3]?.quantity || 0]
+
+  updateGraph(): void {
+    this.barChartData.datasets[0].data = [this.sales[0].quantity];
+    this.barChartData.datasets[1].data = [this.sales[1].quantity];
+    this.barChartData.datasets[2].data = [this.sales[2].quantity];
+    this.barChartData.datasets[3].data = [this.sales[3].quantity];
     if (this.chart) {
       this.chart.update();
     }
   }
-
-
-
 }
-

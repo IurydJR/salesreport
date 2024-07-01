e21294bdf423faf2973c93f5c9100dc09773c2c4
IurydJR <iury.djr.ti@gmail.com>
2024-07-01 02:45:53 -0300

    feat:Added functionality to navigate to the login page when clicking user on nav bar

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -8,7 +8,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { User } from '../../interfaces/User';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-main-nav',
@@ -16,6 +18,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './main-nav.component.scss',
   standalone: true,
   imports: [
+    NgIf,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -29,10 +32,32 @@ import { RouterOutlet } from '@angular/router';
 
 export class MainNavComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  private user!: User;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+    constructor(private router: Router) {};
+
+    showUser(): { user: User | null, letter: string } {
+      const userJson = localStorage.getItem('currentUser'); 
+  
+      if (userJson) {
+        this.user = JSON.parse(userJson); 
+        const letter = this.user.name.charAt(0); 
+        return { user: this.user, letter }; 
+      } else {
+        return { user: null, letter: '' }; 
+      }
+    }
+
+    goToLogin(): void {
+      if(!localStorage.getItem('currentUser')) {
+        this.router.navigate(['/login']);
+      }
+    }
+
 }
